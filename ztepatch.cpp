83431f1bbccf9386c83d65160176c03712d664ca
aldertotori <aldertotori@163.com>
2017-11-06 12:00:45 +0800

    zte mu351 win8/10 patch

---
+++ ztepatch.cpp
@@ -0,0 +1,15 @@
+// ztepatch.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+	printf("Hello World!\n");
+
+
+
+
+	return 0;
+}
+
